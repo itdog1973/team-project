8fbd1d42232fb6c9eb8513e579550431f0d28eb0
jenniehuang <ting1236t19@gmail.com>
2022-04-20 02:11:29 +0800

    add weather imgs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import "./scss/style.scss";
 import { TW_CITIES } from "./utils/CITY_LIST";
+import sunny from "./images/sun.png";
+import rainy from "./images/rainy.gif";
+import cloudyMoon from "./images/cloudy.png";
+import cloudySun from "./images/cloudy_sun.png";
 
 function App() {
   const [location, setLocation] = useState("%E8%87%BA%E5%8C%97%E5%B8%82");
@@ -20,7 +24,7 @@ function App() {
         },
       });
       let data = await response.json();
-      console.log(data);
+
       setWxArr(data.records.location[0].weatherElement[0].time);
       setPoPArr(data.records.location[0].weatherElement[1].time);
       setMinTArr(data.records.location[0].weatherElement[2].time);
@@ -42,14 +46,29 @@ function App() {
         <h1>Future weather in :</h1>
         <select onChange={submitHandler} name="region" id="region">
           {TW_CITIES.map((v) => (
-            <option value={v}>{v}</option>
+            <option key={v} value={v}>
+              {v}
+            </option>
           ))}
         </select>
       </header>
       <div className="weather__container">
         {WxArr.map((v, i) => (
           <div className="weather" key={v.startTime}>
-            <div className="weather__pic"><img src={require("./assets/images/sun.png")} alt="" /></div>
+            <div className="weather__pic">
+              <img
+                src={
+                  v.parameter.parameterValue > 7
+                    ? rainy
+                    : v.parameter.parameterValue === 1
+                    ? sunny
+                    : v.startTime.split(" ")[1] === "06:00:00"
+                    ? cloudySun
+                    : cloudyMoon
+                }
+                alt=""
+              />
+            </div>
             <div className="weather__txt">
               <div>開始時間：{v.startTime}</div>
               <div>結束時間：{v.endTime}</div>
